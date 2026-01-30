458996e9a567d738a5f7a0398f9ad8499aa7971d
akashmhadgut <akashmhadgut280@gmail.com>
2026-01-30 13:58:10 +0530

    Remove backend to split into separate repository

--- backend/src/controllers/index.js
+++
@@ -1,3 +0,0 @@
-export * from './authController.js';
-export * from './statusController.js';
-export * from './orderController.js';
--- backend/src/models/index.js
+++
@@ -1,5 +0,0 @@
-import User from './User.js';
-import Status from './Status.js';
-import Order from './Order.js';
-
-export { User, Status, Order };
